0af12451ee14e3a3c7bcf2a8a11734eba44d23e7
Olivia Fernandez <olifer97@gmail.com>
2018-11-01 18:25:56 -0300

    TP2. Falta como tomar el char del valor que nos dan en el archivo y como medir los tiempos de ejecucion para comparar.

--- tp2/cache.h
+++ tp2/cache.h
@@ -7,29 +7,40 @@
 /* *****************************************************************
  *                DEFINICION DE LOS TIPOS DE DATOS
  * *****************************************************************/
-
+typedef struct cache cache_t;
 
 
 /* *****************************************************************
  *                    PRIMITIVAS DE LA CACHE
  * *****************************************************************/
 
+//Inicializa bloques de la cache como invalidos.
+//tasa de misses = 0.
 void init();
 
+//Devuelve el conjunto al que mapea la direccion
+//address.
 int find_set(int address);
 
+//Devuelve el bloque menos reciencientemenes usado.
 int find_lru(int setnum);
 
+//Devuelve el estado del bit Dirty del bloque.
 int is_dirty(int way, int setnum);
 
+//Leer el bloque pasado por parametro y guardarlo en la cache
 void read_block(int blocknum);
 
-void write_block(int address);
+//Escribe los datos del bloque setnum  de la via way
+void write_block(int way, int setnum);
 
+//Devolver el valor correspondiente a la posicion address
 int read_byte(int address);
 
+//Escribe value en la pos correcta del bloque correspondiente a address
 int write_byte(int address, char value);
 
+//Devuelve el porcentaje de misses.
 int get_miss_rate();
 
 
--- tp2/main.c
+++ tp2/main.c
@@ -5,10 +5,91 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <stddef.h>
-#include <errno.h>
 
 
 #include "cache.h"
+
+//TODO: Medir tiempos de ejecucion para despues poder comparar.
+
+void realizar_read(char* direccion){
+
+
+   if(!direccion){
+     printf("Falta la direccion de memoria a leer.\n");
+     return;
+   }
+
+   //Habria que verificar que es realmente un numero.
+   int address = atoi(direccion); //convierto la direccion en int
+
+   printf("Read devuelve: %d\n", read_byte(address));
+}
+
+
+void realizar_write(char* direccion, char* valor){
+
+  //Habria que verificar que es realmente un numero.
+  int address = atoi(direccion);
+
+  if(!valor){
+    printf("Falta el valor a escribir.\n");
+  }
+
+  //TODO: No se como pasar el valor de char* a char
+  //printf("el char* value es %s\n",valor);
+  char value = *valor;//esto me toma solo el primer valor y vienen 3
+  printf("Write devuelve: %d\n",write_byte(address, value));
+}
+
+
+bool procesar_archivo(const char* nombre){
+  FILE* archivo = fopen(nombre,"r");
+  if(!archivo){
+    fprintf (stderr,"Error al tratar de abrir archivo.\n");
+    return false;
+  }
+  char* linea = NULL; size_t capacidad = 0; ssize_t leidos;
+  while((leidos = getline(&linea,&capacidad,archivo)) > 0){
+    if(strcmp(linea, "MR\n") == 0){
+      printf("Miss_rate es: %d\n", get_miss_rate());
+    }
+    char* comando = strtok(linea, " ");
+    if(strcmp(comando, "R") == 0){
+      char* direccion = strtok(NULL, " ");
+      realizar_read(direccion);
+    }
+    if(strcmp(comando, "W") == 0){
+      char* direccion = strtok(NULL, " ");
+      if(!direccion){
+        printf("Falta direccion de memoria en donde escribir.\n");
+      }
+      char* valor = strtok(NULL, " ");
+      realizar_write(direccion, valor);
+    }
+
+  }
+  free(linea);
+  fclose(archivo);
+  return true;
+}
+
+
+int main(int argc, char *argv[]){
+  if (argc != 2){
+    fprintf (stderr,"Cantidad de parámetros incorrecta.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  //TODO: iniciamos sistema
+  init();
+
+  if(!procesar_archivo(argv[1])){
+    fprintf (stderr,"Error al procesar el archivo.\n");
+    exit(EXIT_FAILURE);
+  }
+
+
+    return 0;
+}
